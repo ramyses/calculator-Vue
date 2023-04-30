5cf903391cfaea02f7b1023c347a3cf64e36f640
ramyses <lukasramysesleao@gmail.com>
2023-04-29 23:38:37 -0400

    components create

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,43 @@
 <template>
     <div id="app">
-        <h1>Olá Vue!</h1>
+        <h1>CAlculadora</h1>
+        <Calculator />
     </div>
 </template>
 
 <script>
-export default {
+    import Calculator from "./main/Calculator.vue"
 
+export default {
+   components: { Calculator}
 }
 </script>
-
+    
 <style>
+    @font-face {
+        font-family: "RobotoMono" ;
+        src: url(./fonts/RobotoMono-Thin.ttf)
+    }
 
+    * {
+        font-family: "RobotMono", monospace;
+    }
+
+    body {
+        margin: 0;
+    }
+
+    #app {
+        display: flex;
+        flex-direction: column;
+        height: 100vh;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+
+        color: #fff;
+        background: linear-gradient(to right, rgb(83, 105, 118), rgb(41, 46, 73));
+
+        
+    }
 </style>
